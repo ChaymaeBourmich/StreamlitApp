cd03b64559a1136e1fbfbfd9e743dfb09c68781a
Chaymae Bourmich <chaymae.bourmich@etu.uae.ac.ma>
2024-12-02 22:16:43 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m streamlit run updated_interface1.py --server.port 8080 --server.address 0.0.0.0
+python -m streamlit run updated_interface1.py --server.port 8501 --server.address 0.0.0.0
